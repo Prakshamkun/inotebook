a5e6385dc7d8edd2fc63b5a878629ae63f1e8102
praksham <prakshamkushwaha@gmail.com>
2026-06-19 22:06:27 +0530

    Prepare backend for Render

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook";
-
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to Mongo Successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Connected To MongoDB Atlas");
   } catch (error) {
-    console.error("MongoDB Connection Error:", error);
+    console.error(error);
   }
 };
 
